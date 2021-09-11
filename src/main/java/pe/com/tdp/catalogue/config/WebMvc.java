512e3589ad6a6706843a88d6917af676a6609ce5
Elvis Perez <ebardalesdj@mpfn.gob.pe>
2021-09-10 19:45:22 -0500

    Integracion con swagger

---
+++ src/main/java/pe/com/tdp/catalogue/config/WebMvc.java
@@ -0,0 +1,43 @@
+package pe.com.tdp.catalogue.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class WebMvc {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pe.com.tdp.catalogue"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Catalogue Service API",
+                "Evaluation API Catalogue",
+                "1.0",
+                "http://codmind.com/terms",
+                new Contact("TDP Evaluation", "https://tdp.com/api/evaluation", "apis@tdp.com"),
+                "Not Found",
+                "Not Found",
+                Collections.emptyList()
+        );
+    }
+
+}
--- src/main/java/pe/com/tdp/catalogue/model/Mobile.java
+++ src/main/java/pe/com/tdp/catalogue/model/Mobile.java
@@ -33,8 +33,8 @@ public class Mobile {
     @ManyToMany
     @JoinTable(
             name = "mobile_ofert",
-            joinColumns = @JoinColumn(name = "ofertFk"),
-            inverseJoinColumns = @JoinColumn(name = "mobileFk")
+            joinColumns = @JoinColumn(name = "mobileFk"),
+            inverseJoinColumns = @JoinColumn(name = "offertFk")
     )
     private List<Offert> offerts;
 
